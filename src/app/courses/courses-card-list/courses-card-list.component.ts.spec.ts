d9a943d438fb39aab53962106af56d77a06c6d8b
Faïzou Aremou <faizouaremou@gmail.com>
2023-11-23 14:04:19 +0100

    debug presentation components

--- src/app/courses/courses-card-list/courses-card-list.component.ts.spec.ts
+++ src/app/courses/courses-card-list/courses-card-list.component.ts.spec.ts
@@ -2,25 +2,37 @@ import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 import { CoursesCardListComponent } from "./courses-card-list.component";
 import { CoursesModule } from "../courses.module";
 import { COURSES } from "../../../../server/db-data";
-import { CUSTOM_ELEMENTS_SCHEMA, DebugElement } from "@angular/core";
 import { By } from "@angular/platform-browser";
 import { sortCoursesBySeqNo } from "../home/sort-course-by-seq";
 import { Course } from "../model/course";
 import { setupCourses } from "../common/setup-test-data";
+import { DebugElement } from "@angular/core";
 
 describe("CoursesCardListComponent", () => {
-  beforeEach(() => {
+  let component: CoursesCardListComponent;
+  let fixture: ComponentFixture<CoursesCardListComponent>;
+  let debugElement: DebugElement;
+  beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [CoursesModule],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA],
-    });
-  });
+    })
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(CoursesCardListComponent);
+        component = fixture.componentInstance;
+        debugElement = fixture.debugElement;
+      });
+  }));
   it("should create the component", () => {
-    pending();
+    expect(component).toBeTruthy();
   });
 
   it("should display the course list", () => {
-    pending();
+    const coursesData = setupCourses();
+    component.courses = coursesData;
+    const cardElements = debugElement.queryAll(By.css(".course-card"));
+    expect(cardElements).toBeTruthy("Could not find all card Element");
+    expect(cardElements.length).toBe(coursesData.length, "unexpected number of courses");
   });
 
   it("should display the first course", () => {
