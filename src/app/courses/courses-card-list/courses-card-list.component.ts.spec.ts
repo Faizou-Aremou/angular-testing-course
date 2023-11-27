a7f5223977c72d4dfe0481aefb3fd17bc24fe214
Faïzou Aremou <faizouaremou@gmail.com>
2023-11-27 13:35:27 +0100

    feat: finalize presentational component testing

--- src/app/courses/courses-card-list/courses-card-list.component.ts.spec.ts
+++ src/app/courses/courses-card-list/courses-card-list.component.ts.spec.ts
@@ -1,7 +1,6 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 import { CoursesCardListComponent } from "./courses-card-list.component";
 import { CoursesModule } from "../courses.module";
-import { COURSES } from "../../../../server/db-data";
 import { By } from "@angular/platform-browser";
 import { sortCoursesBySeqNo } from "../home/sort-course-by-seq";
 import { Course } from "../model/course";
@@ -27,15 +26,34 @@ describe("CoursesCardListComponent", () => {
     expect(component).toBeTruthy();
   });
 
-  it("should display the course list", () => {
-    const coursesData = setupCourses();
-    component.courses = coursesData;
-    const cardElements = debugElement.queryAll(By.css(".course-card"));
-    expect(cardElements).toBeTruthy("Could not find all card Element");
-    expect(cardElements.length).toBe(coursesData.length, "unexpected number of courses");
-  });
+  describe("test courses", () => {
+    let coursesData: Course[];
+    beforeEach(() => {
+      coursesData = setupCourses();
+      component.courses = coursesData;
+      fixture.detectChanges();
+    });
+    it("should display the course list", () => {
+      const cardElements = debugElement.queryAll(By.css(".course-card"));
+      expect(cardElements).toBeTruthy("Could not find all card Element");
+      expect(cardElements.length).toBe(
+        coursesData.length,
+        "unexpected number of courses"
+      );
+    });
 
-  it("should display the first course", () => {
-    pending();
+    it("should display the first course", () => {
+      const firstCourse = component.courses[0];
+      const cardElement = debugElement.query(
+        By.css(".course-card:first-child")
+      );
+      const firstCourseTitleElement = cardElement.query(By.css("mat-card-title"));
+      const firstCourseImgElement = cardElement.query(By.css("img"));
+      expect(cardElement).toBeTruthy();
+      expect(firstCourseTitleElement.nativeElement.textContent).toBe(
+        firstCourse.titles.description
+      );
+      expect(firstCourseImgElement.nativeElement.src).toBe(firstCourse.iconUrl);
+    });
   });
 });
