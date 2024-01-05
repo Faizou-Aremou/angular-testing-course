65fe3985e2e1b7403af9bb583fc850955f62663a
Faizou <faizouaremou@gmail.com>
2024-01-06 00:50:08 +0100

    test micro task like promise

--- src/app/courses/home/async-exemples.spec.ts
+++ src/app/courses/home/async-exemples.spec.ts
@@ -1,5 +1,4 @@
-import { fakeAsync, flush, tick } from "@angular/core/testing";
-import { doesNotReject } from "assert";
+import { fakeAsync, flush, flushMicrotasks } from "@angular/core/testing";
 
 describe("Describe async test example", () => {
   it("Asynchronous test example", (done) => {
@@ -20,6 +19,20 @@ describe("Describe async test example", () => {
       test = true;
     }, 1000);
     flush();
+    expect(test).toBeTruthy();
+  }));
+  fit("Asynchronous test example- plain example", fakeAsync(() => {
+    let test = false;
+    console.log("creating promise");
+    Promise.resolve().then(() => {
+      console.log("Promise evaluated successfully");
+      test = true;
+      return Promise.resolve().then(() => {
+        console.log("Second then block Promise evaluated succesfully");
+      });
+    });
+    flushMicrotasks();
+    console.log("Running test assertions");
     expect(test).toBeTruthy();
   }));
 });
